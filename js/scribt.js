75c4bc33850ce3efbebb9d453e2d64aa356746ac
abelrhmankakapo2-png <abelrhmankakapo2@gmail.com>
2025-10-05 14:34:04 +0300

    Add files via upload

---
+++ js/scribt.js
@@ -0,0 +1,20 @@
+document.getElementById('validationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const id = document.getElementById('id').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (username === '29212022100010' && id === '30406101306594') {
+        window.location.href = 'success.html';
+    }
+        else if (username === '29112024100738' && id === '30608121300713') {
+        window.location.href = 'success2.html';
+    }
+    else {
+        errorMessage.textContent = 'كودالطالب او كلمة السر غير صحيحه';
+        errorMessage.style.display = 'block';
+    }
+});
+
+
